cb895623143832ba7daaf6f85a51a82135f25407
AnnaBiro <81937689+AnnaBiro@users.noreply.github.com>
2021-07-05 14:18:14 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
  export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+       
+     <footer>
+       This project was coded by Anna Biro and is {" "}
+       <a href="https://github.com/AnnaBiro/react-weather" target="_blank" rel="noreferrer">
+         open-sourced on GitHub.
+
+     </a>
+     </footer>
+     
     </div>
   );
 }
